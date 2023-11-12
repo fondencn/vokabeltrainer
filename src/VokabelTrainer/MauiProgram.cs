cac754faa3a3231fca65825d85785e02c471be17
Christian Fonden <c.fonden@die-rooter.de>
2023-11-12 20:03:02 +0100

    App Icons anpassen

--- src/VokabelTrainer/MauiProgram.cs
+++ src/VokabelTrainer/MauiProgram.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using VokabelTrainer.Model;
-using VokabelTrainer.Model.Api;
+﻿using Microsoft.Extensions.Logging;
 using VokabelTrainer.Services;
-using VokabelTrainer.View;
 
 namespace VokabelTrainer;
 
